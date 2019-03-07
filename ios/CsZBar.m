1eb25724d5308076614203bf00dc6f8fa8eeb888
Nuno Miguel Verdasca <miguel.f.verdasca@gmail.com>
2019-03-07 10:22:03 +0000

    Update CsZBar.m

--- ios/CsZBar.m
+++ ios/CsZBar.m
@@ -81,11 +81,9 @@
         //UIView *infoButton = [[[[[self.scanReader.view.subviews objectAtIndex:2] subviews] objectAtIndex:0] subviews] objectAtIndex:infoButtonIndex];
         //[infoButton setHidden:YES];
 
-        
-        // MIKE MIKE put value of objectAtIndex 2 -> 0 | lines 86 & 88
-        NSArray *viewArray = [[[[[[[[self.scanReader.view.subviews objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews];
+        NSArray *viewArray = [[[[[[[[self.scanReader.view.subviews objectAtIndex:2] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews];
         if([viewArray count] > 0){
-            UIView *infoButton = [[[[[[[[[self.scanReader.view.subviews objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:infoButtonIndex];
+            UIView *infoButton = [[[[[[[[[self.scanReader.view.subviews objectAtIndex:2] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:infoButtonIndex];
             [infoButton setHidden:YES];
         }
 
